f6934f8649d4c9ce7b3f5f68f5961dde19ed6329
dodabear42 <lhicks42@gmail.com>
2014-11-04 19:57:27 -0500

    css and content updates
    add pages and css across

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('#teacher-login').click(function() {
+		$('#parent-login').css('background-color', '#369').css('color', '#000');
+		$('#teacher-login').css('background-color', '#24476b').css('color', '#fff');
+		$('#parent-login-wrapper').hide();
+		$('#teacher-login-wrapper').show();
+	});
+	$('#parent-login').click(function() {
+		$('#teacher-login').css('background-color', '#369').css('color', '#000');
+		$('#parent-login').css('background-color', '#24476b').css('color', '#fff');
+		$('#parent-login-wrapper').show();
+		$('#teacher-login-wrapper').hide();
+	});
+	$('#teacher-register').click(function() {
+		$('#parent-register').css('background-color', '#369').css('color', '#000');
+		$('#teacher-register').css('background-color', '#24476b').css('color', '#fff');
+		$('#parent-register-wrapper').hide();
+		$('#teacher-register-wrapper').show();
+	});
+	$('#parent-register').click(function() {
+		$('#teacher-register').css('background-color', '#369').css('color', '#000');
+		$('#parent-register').css('background-color', '#24476b').css('color', '#fff');
+		$('#parent-register-wrapper').show();
+		$('#teacher-register-wrapper').hide();
+	});
+});
