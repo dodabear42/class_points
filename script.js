57db8fc588a2dcab5fbef14301bd6e4cffc65a3e
dodabear42 <lhicks42@gmail.com>
2017-02-13 13:55:15 -0500

    add and style app
    Add and style app portion of program

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ $(document).ready(function() {
 		$('#teacher-register-wrapper').hide();
 	});
 });
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
